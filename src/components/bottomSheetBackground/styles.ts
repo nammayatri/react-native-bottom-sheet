bc87f991d7af4b066d64940ed6cd2e38d411c859
Pravinkumar S <spravinkumar9952@gmail.com>
2024-12-16 14:08:00 +0530

    fix: remove accessiblity

--- src/components/bottomSheetBackground/styles.ts
+++ src/components/bottomSheetBackground/styles.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   container: StyleSheet.absoluteFillObject,
   background: {
-    backgroundColor: 'white',
+    backgroundColor: 'transparent',
     borderRadius: 15,
   },
 });
