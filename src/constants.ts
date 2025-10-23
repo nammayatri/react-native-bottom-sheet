e481435bd4c48bf0948740e8b8b96dce410cbc8c
Dev Vikram <harleysam12@gmail.com>
2025-10-24 04:32:24 +0530

    feat: smoother animations

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
 import { Dimensions, Platform } from 'react-native';
-import type Animated from 'react-native-reanimated';
 import { Easing } from 'react-native-reanimated';
 import type { SpringConfig, TimingConfig } from './types';
 
@@ -69,22 +68,22 @@ enum SNAP_POINT_TYPE {
   DYNAMIC = 1,
 }
 
-const ANIMATION_EASING: Animated.EasingFunction = Easing.out(Easing.exp);
-const ANIMATION_DURATION = 250;
+const ANIMATION_EASING = Easing.bezier(0.3, 0.1, 0.55, 1);
+const ANIMATION_DURATION = 270;
 
 const ANIMATION_CONFIGS = Platform.select<TimingConfig | SpringConfig>({
-  android: {
+  default: {
     duration: ANIMATION_DURATION,
     easing: ANIMATION_EASING,
   },
-  default: {
-    damping: 500,
-    stiffness: 1000,
-    mass: 3,
-    overshootClamping: true,
-    restDisplacementThreshold: 10,
-    restSpeedThreshold: 10,
-  },
+  // default: {
+  //   damping: 500,
+  //   stiffness: 1000,
+  //   mass: 3,
+  //   overshootClamping: true,
+  //   restDisplacementThreshold: 10,
+  //   restSpeedThreshold: 10,
+  // },
 });
 
 const SCROLLABLE_DECELERATION_RATE_MAPPER = {
