513b33442532143c06b96486f88395524e6043e7
Dev Vikram <harleysam12@gmail.com>
2025-11-12 20:13:28 +0530

    fix: background bottomsheet is set to transparent

--- src/components/bottomSheetBackground/styles.ts
+++ src/components/bottomSheetBackground/styles.ts
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
-    borderRadius: 15,
+    backgroundColor: 'transparent',
+    borderRadius: 20,
   },
 });
